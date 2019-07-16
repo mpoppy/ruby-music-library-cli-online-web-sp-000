de26612184a86b1ee39fd961424b4cdc84362ca3
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 03:33:38 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -11,6 +11,7 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list artists'."
     input = gets.chomp
   end
 end
