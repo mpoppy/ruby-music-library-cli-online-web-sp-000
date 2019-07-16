41701f687bae897d68f2dfe4323e8de6ce6687ea
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 04:46:38 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,7 +20,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
-      input = gets.chomp
+      input = gets.strip
         if input == "list songs"
           list_songs
         elsif input == "list artists"
