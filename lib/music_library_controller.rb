f04056ee27354f4991b8172901c5db1852db124c
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 05:43:53 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,8 +41,8 @@ class MusicLibraryController
   def list_songs
       Song.all.sort{ |a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
         puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-        binding.pry
       end
+      binding.pry
   end
 
   def list_artists
