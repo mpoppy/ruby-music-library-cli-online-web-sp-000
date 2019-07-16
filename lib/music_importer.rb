fd65c12b6e4fa3f0a2d0c86b2ccde7e5f1a11be8
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 03:16:56 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,15 +1,18 @@
 
 class MusicImporter
-  
+
   attr_accessor :path
-  
+
   def initialize(path)
     @path = path
   end
-  
+
   def files
     Dir.chdir(@path) do path
       Dir.glob("*.mp3")
     end
+  end
+  def import
+
   end
 end
