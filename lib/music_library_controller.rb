31263b9fc52caa79df6f2fcb83c8b8bac18c27e3
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 05:23:58 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -78,7 +78,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
-    list_songs.detect{|song, index| song[index] = input}
+    list_songs.detect{|song, index| song[index] = input puts song}
 
   end
 
