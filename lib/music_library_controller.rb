052c0d175a42a9569f3c1c355b6e1a315659a83f
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 04:06:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,7 +5,7 @@ class MusicLibraryController
 
   def initialize(path = './db/mp3s')
     self.path = path if path
-    MusicImporter.new(path).import
+    music = MusicImporter.new(path).import
   end
 
   def call
