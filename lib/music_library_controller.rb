1574650a692e1f36eb69698e25ad5e3824551715
Marc Poppy <poppy.marc@gmail.com>
2019-07-16 03:51:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,16 +21,17 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.chomp
-      if input == "list songs"
-        list_songs
-      elsif input == "list artists"
-        list_artists
-      elsif input == "list genres"
-        list_genres
-      elsif input == "list artist"
-        list_songs_by_artist
-      elsif input == "list genre"
-        list_songs_by_genre
+        if input == "list songs"
+          list_songs
+        elsif input == "list artists"
+          list_artists
+        elsif input == "list genres"
+          list_genres
+        elsif input == "list artist"
+          list_songs_by_artist
+        elsif input == "list genre"
+          list_songs_by_genre
+        end
     end
   end
 
